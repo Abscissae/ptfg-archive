f9de53ad3d66942d2a0044b9b0926bd12d86452f
Abscissae <75873104+Abscissae@users.noreply.github.com>
2022-06-21 15:52:24 +0100

    Slight javascript improvements
    Use let and const in place of var and === instead of ==

--- archive.js
+++ archive.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  var H = location.hash.substr(1);
+  let H = location.hash.substr(1);
   if (document.getElementById(H) || H.trim() == "") return;
   H=decodeURI(H);  
   document.getElementById("storysearch").value = H;
@@ -9,18 +9,18 @@ document.addEventListener("DOMContentLoaded", function(event) {
 function search(H)
 {
   H = H.trim().toLowerCase();
-  if (H.length == 0)
+  if (H.length === 0)
   {
     displayAll();
     return;
   }
   
-  var A = document.querySelectorAll(document.getElementById("sortedstories").hasChildNodes()?"#sortedstories":"main section");
-  var d = 0;
+  const A = document.querySelectorAll(document.getElementById("sortedstories").hasChildNodes()?"#sortedstories":"main section");
+  let d = 0;
   for (i = 0; i < A.length; i++)
   {
-    var S = A[i].getElementsByTagName("article");
-    var c = 0;
+    const S = A[i].getElementsByTagName("article");
+    let c = 0;
     
     for (j = 0; j < S.length; j++)
     {
@@ -33,9 +33,9 @@ function search(H)
         S[j].style.display="none";
     }
     d+=c;
-    A[i].style.display=(c==0)?"none":"";
+    A[i].style.display=(c===0)?"none":"";
   }
-  if (d==0)
+  if (d===0)
     displayAll();
   document.getElementById("numstories").innerText = d+"/";
 }
@@ -53,22 +53,22 @@ function checkStory(story, search)
 
 function sortBy(v)
 {
-	var N = document.getElementById("sortedstories");
-	var M = document.getElementById("storiesbyauthor");
+	const N = document.getElementById("sortedstories");
+	const M = document.getElementById("storiesbyauthor");
 	N.replaceChildren();
-	if (v=="Author")
+	if (v==="Author")
 	{
 		M.style.display="";
 		search(document.getElementById("storysearch").value);
 		return;
 	}
 	
-	var s = document.querySelectorAll("#storiesbyauthor article");
+	const s = document.querySelectorAll("#storiesbyauthor article");
 	M.style.display="none";
 
-	if (v=="Old")
+	if (v==="Old")
 		[...s].sort((a,b)=>a.querySelector("time").innerText>b.querySelector("time").innerText?1:-1).forEach(n=>N.appendChild(n.cloneNode(true)));
-	else if (v=="New")
+	else if (v==="New")
 		[...s].sort((a,b)=>a.querySelector("time").innerText<b.querySelector("time").innerText?1:-1).forEach(n=>N.appendChild(n.cloneNode(true)));
 	search(document.getElementById("storysearch").value);
 }
